52d8ce979e242bbb01f44fbc3e33bb1dc322b3e5
LoveKumarAgrawal <loveagrawal204@gmail.com>
2024-12-19 19:11:58 +0530

    added images and made auth layout

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+  return <div>Login Page</div>;
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+  return <div>Login Page</div>;
+};
+
+export default SignUp;
