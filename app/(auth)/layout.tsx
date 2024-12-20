aaf23270c0c891a24c9a04d19751a31537673633
LoveKumarAgrawal <loveagrawal204@gmail.com>
2024-12-20 22:10:36 +0530

    added reusable authform

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
       <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
-        <div className="max-h max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
           <Image
             src="/assets/icons/logo-full.svg"
             alt="logo"
@@ -13,6 +13,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             height={82}
             className="h-auto"
           />
+
           <div className="space-y-5 text-white">
             <h1 className="h1">Manage your files the best way</h1>
             <p className="body-1">
@@ -28,7 +29,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           />
         </div>
       </section>
-      <section className="lg-py-0 flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10">
+
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
             src="/assets/icons/logo-full-brand.svg"
@@ -38,8 +40,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             className="h-auto w-[200px] lg:w-[250px]"
           />
         </div>
+
+        {children}
       </section>
-      {children}
     </div>
   );
 };
